278db9b18819a9692b2642db04772daa1a0dd15a
sandnine <sandnine@gmail.com>
2020-05-04 21:29:21 -0300

    Modificacoes
    Aviao modificado

--- Assets/Scripts/Aviao.cs
+++ Assets/Scripts/Aviao.cs
@@ -7,7 +7,7 @@ public class Aviao : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //123456
     }
 
     // Update is called once per frame
